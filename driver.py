fa646f2a2a0a56833dd15cf410849ab06b47df18
Divyam Tembhare <109894984+divyamth@users.noreply.github.com>
2023-04-10 19:56:39 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -14,7 +14,7 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	df= pd.read_csv("spam.csv", encoding="latin-1")
+	df= pd.read_csv("datsets.csv", encoding="latin-1")
 	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
 	# Features and Labels
 	df['label'] = df['class'].map({'ham': 0, 'spam': 1})
