8e506c7948ee9e3a07f864fcd9315733f2ae92fc
Divyam Tembhare <109894984+divyamth@users.noreply.github.com>
2023-04-26 23:37:41 +0530

    Update driver.py

--- driver.py
+++ driver.py
@@ -10,7 +10,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-	return render_template('home.html')
+	return render_template('main.html')
 
 @app.route('/predict',methods=['POST'])
 def predict():
